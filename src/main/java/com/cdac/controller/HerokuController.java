8b1206285ac2d98f3845a624df6ba3a213f7c5f7
sathishmepco53@gmail.com <sathish@greenboards.in>
2018-04-11 14:15:01 +0530

    simple json rest service is added

---
+++ src/main/java/com/cdac/controller/HerokuController.java
@@ -0,0 +1,24 @@
+package com.cdac.controller;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HerokuController {
+	
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@RequestMapping(value = "/call", method = RequestMethod.POST,headers = "Accept=application/json", produces = "application/json",consumes="application/json")
+	public @ResponseBody String getInput(@RequestBody String request){
+		System.out.println("Inside Call");
+		System.out.println("Request Received is :: "+request);
+		return "{	color: \"red\",	value: \"#f00\"}";
+	}
+}
