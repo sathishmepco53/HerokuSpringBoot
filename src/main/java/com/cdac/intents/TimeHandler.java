2fa7692192dd33c9364616cfd5cfc3ea1f954f97
sathishmepco53@gmail.com <sathish@greenboards.in>
2018-04-12 02:42:44 +0530

    time

--- src/main/java/com/cdac/intents/TimeHandler.java
+++ src/main/java/com/cdac/intents/TimeHandler.java
@@ -18,8 +18,12 @@ public class TimeHandler {
 	public String process(){
 		JSONObject jsonObject = new JSONObject();
 		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Calcutta"));
-		SimpleDateFormat sdf = new SimpleDateFormat("hh mm");
+		SimpleDateFormat sdf = new SimpleDateFormat("hh mm a");
 		String time = sdf.format(new Date());
+		if(time.trim().startsWith("00"))
+			time = time.replace("00", "12");
+		else if(time.trim().startsWith("0"))
+			time = time.replace("0", "\\s");
 		jsonObject.put("fulfillmentText", time);
 		return jsonObject.toString();
 	}
